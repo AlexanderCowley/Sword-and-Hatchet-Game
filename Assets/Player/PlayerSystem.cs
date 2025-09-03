0885eddbf5fc127a6b2e6310f09d0ede61a3f59c
Alec <movieornasasim@gmail.com>
2025-09-03 13:22:13 -0500

    Basic Player Movement and Camera functional

--- Assets/Player/PlayerSystem.cs
+++ Assets/Player/PlayerSystem.cs
@@ -1,6 +1,50 @@
+using Unity.VisualScripting;
 using UnityEngine;
+
+public struct PlayerData
+{
+    public float PlayerSpeed;
+    public Vector3 PlayerVelocity;
+    public Vector3 PlayerPosition;
+
+    public PlayerData(float Speed, Vector3 Velocity, Vector3 Position)
+    {
+        PlayerSpeed = Speed;
+        PlayerPosition = Position;
+        PlayerVelocity = Velocity;
+    }
+
+}
 
 public class PlayerSystem : MonoBehaviour
 {
-    
+
+    public PlayerData PlayerMovementData;
+    Transform PlayerTransform;
+    Rigidbody rigidbody;
+    Quaternion rotation;
+    public void Awake()
+    {
+        PlayerTransform = transform;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        PlayerMovementData = new PlayerData(15f, rigidbody.linearVelocity, transform.position);
+    }
+
+    void UpdateCamera()
+    {
+        //
+    }
+
+    public void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) *
+         PlayerMovementData.PlayerSpeed * Time.deltaTime;
+
+        
+        UpdateCamera();
+    }
 }
