8bce6424f938fe820268220da806f7b209b19d3e
Alec <movieornasasim@gmail.com>
2025-09-24 22:43:36 -0500

    An attack animation plays when either attack buttons are clicked

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Manager;
+
+    public static Animator PlayerAnimator;
     public static int ComboCount;
 
     //Player
     //public static PlayerData PlayerMovement = new PlayerData(15f);
     public static PlayerInput PlayerInputStatic = new();
+    public void AnimResetToIdle()
+    {
+        if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        if (PlayerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.99f)
+        {
+            PlayerAnimator.Play("Idle");
+        }
+    }
     void OnEnable()
     {
         if (Manager == null)
@@ -15,5 +28,13 @@ public class GameManager : MonoBehaviour
             Manager = GetComponent<GameManager>();
         }
         DontDestroyOnLoad(this);
+        PlayerAnimator = FindAnyObjectByType<PlayerSystem>().transform.GetChild(5).GetComponent<Animator>();
     }
+
+    void Update()
+    {
+        //AnimResetToIdle();
+    }
+
+
 }
