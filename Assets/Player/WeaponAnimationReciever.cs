754ae79a36c53cf7a4b4110553fe85549f449c94
Alec <movieornasasim@gmail.com>
2026-02-12 22:13:27 -0600

    Fixed bug that prevented hitboxes from becoming active.

--- Assets/Player/WeaponAnimationReciever.cs
+++ Assets/Player/WeaponAnimationReciever.cs
@@ -5,21 +5,18 @@ public class WeaponAnimationReciever : MonoBehaviour
     //Move Hitbox to CombatController.
     [SerializeField] HitBox hitBox;
     [SerializeField] Transform hitboxSpawnpoint;
-    float AttackTimer = 0f;
-
-    float HitboxTimer = 0.5f;
     int HitboxCounter = 0;
     HitBox currentHitbox;
     public void SpawnHitbox()
     {
+        //The problem is here!
         //Should be something like currentHitbox = CombatController.Instance.GetNextHitbox
-        if(GameManager.PlayerHitboxes.Length - 1 > HitboxCounter)
+        if(GameManager.PlayerHitboxes.Length - 1 <= HitboxCounter)
         {
-            //Debug.Log("Max Hitboxes Met");
+            Debug.LogWarning("Max Hitboxes Met");
             return;
         }
         currentHitbox = GameManager.PlayerHitboxes[HitboxCounter];
-        Debug.Log(currentHitbox, currentHitbox.gameObject);
         currentHitbox.gameObject.SetActive(true);
         HitboxCounter++;
     }
@@ -27,5 +24,6 @@ public class WeaponAnimationReciever : MonoBehaviour
     public void AttackFinished()
     {
         CombatController.Instance.FinishAttack();
+        HitboxCounter = 0;
     }
 }
